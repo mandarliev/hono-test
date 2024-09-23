f318cdf8cff49135228488134cbbd6f13ee78d79
Dimitar <dimitar.mandarliev@gmail.com>
2024-09-23 16:35:38 +0300

    Final auth setup

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,15 +1,46 @@
-import { Hono } from "hono";
+import { Hono, Context } from "hono";
+import {
+  authHandler,
+  initAuthConfig,
+  verifyAuth,
+  type AuthConfig,
+} from "@hono/auth-js";
+import GitHub from "@auth/core/providers/github";
 import { handle } from "hono/vercel";
+
+// Basically following this guide
+// https://github.com/honojs/middleware/tree/main/packages/auth-js#authjs-middleware-for-hono
+
+import todos from "./todos";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 
-app.get("/hello", (c) => {
-  return c.json({
-    message: "Hello Next.js!",
-  });
+app.use("*", initAuthConfig(getAuthConfig));
+app.use("/auth/*", authHandler());
+
+app.get("/protected", verifyAuth(), (c) => {
+  const auth = c.get("authUser");
+
+  return c.json(auth);
 });
+
+app.route("/todos", todos);
+
+function getAuthConfig(c: Context): AuthConfig {
+  return {
+    // Instead of c.env, I have to use process.env
+    // since it was initially set to work w/ Cloudflare workers
+    secret: process.env.AUTH_SECRET,
+    providers: [
+      GitHub({
+        clientId: process.env.GITHUB_ID,
+        clientSecret: process.env.GITHUB_SECRET,
+      }),
+    ],
+  };
+}
 
 export const GET = handle(app);
 export const POST = handle(app);
